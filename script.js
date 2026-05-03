4b9e0af46ea7d40581bdcbc9f3fb91d02f67916d
RealBlacknomad <rcgsoporte@gmail.com>
2026-05-03 18:28:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 resultsDiv.innerHTML = "";
 
+// Abstract principal
 if (data.Heading && data.AbstractText) {
   resultsDiv.innerHTML += `
     <div class="result-item">
       <h2>${data.Heading}</h2>
       <p>${data.AbstractText}</p>
-      ${data.AbstractURL ? `<p><a href="${data.AbstractURL}" target="_blank">Más información</a></p>` : ""}
+      ${data.AbstractURL ? `<a href="${data.AbstractURL}" target="_blank">Más información</a>` : ""}
     </div>
   `;
 }
 
+// RelatedTopics
 if (data.RelatedTopics && data.RelatedTopics.length > 0) {
   data.RelatedTopics.forEach(item => {
     if (item.Text && item.FirstURL) {
       resultsDiv.innerHTML += `
         <div class="result-item">
           <h3><a href="${item.FirstURL}" target="_blank">${item.Text}</a></h3>
-          <p>${item.Text}</p>
         </div>
       `;
     }
