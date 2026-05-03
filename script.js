f91702f4c4b5d12660a2b31bbc17b9b3a5d713a9
RealBlacknomad <rcgsoporte@gmail.com>
2026-05-03 18:27:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-// Mostrar Abstract principal
+resultsDiv.innerHTML = "";
+
 if (data.Heading && data.AbstractText) {
-  const div = document.createElement("div");
-  div.className = "result-item";
-  div.innerHTML = `
-    <h2>${data.Heading}</h2>
-    <p>${data.AbstractText}</p>
-    ${data.AbstractURL ? `<p><a href="${data.AbstractURL}" target="_blank">Más información</a></p>` : ""}
+  resultsDiv.innerHTML += `
+    <div class="result-item">
+      <h2>${data.Heading}</h2>
+      <p>${data.AbstractText}</p>
+      ${data.AbstractURL ? `<p><a href="${data.AbstractURL}" target="_blank">Más información</a></p>` : ""}
+    </div>
   `;
-  resultsDiv.appendChild(div);
 }
 
-// Mostrar RelatedTopics
 if (data.RelatedTopics && data.RelatedTopics.length > 0) {
   data.RelatedTopics.forEach(item => {
     if (item.Text && item.FirstURL) {
-      const div = document.createElement("div");
-      div.className = "result-item";
-      div.innerHTML = `
-        <h3><a href="${item.FirstURL}" target="_blank">${item.Text}</a></h3>
-        <p>${item.Text}</p>
+      resultsDiv.innerHTML += `
+        <div class="result-item">
+          <h3><a href="${item.FirstURL}" target="_blank">${item.Text}</a></h3>
+          <p>${item.Text}</p>
+        </div>
       `;
-      resultsDiv.appendChild(div);
     }
   });
 }
